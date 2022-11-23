5701bfcf49540aa3bde2daab8f995d2efabca60f
linbingjie <linbingjie@itiger.com>
2022-11-23 20:57:04 +0800

    0.4.5

---
+++ TestSwiftOC/Classes/TestSwiftOC-Swift.h
@@ -0,0 +1,12 @@
+//
+//  TestSwiftOC-Swift.h
+//  Pods
+//
+//  Created by linbingjie on 2022/11/23.
+//
+
+#ifndef TestSwiftOC_Swift_h
+#define TestSwiftOC_Swift_h
+
+
+#endif /* TestSwiftOC_Swift_h */
